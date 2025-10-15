b6ae044ea1a3e4a7ecdb48ef97703b14a72b23fb
munna-68 <mahamudmunna68@gmail.com>
2025-10-15 18:44:37 +0600

    adding the form closing and opening functionality using simple js with the removing and adding class

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("working")
+const closeBtn = document.querySelector(".x-btn")
+const formIcon = document.querySelector(".form-icon")
+const formContainer = document.querySelector(".form-container")
+
+formIcon.addEventListener('click', (e) => {
+  formContainer.classList.toggle("hidden")
+})
+
+closeBtn.addEventListener('click', (e) => {
+  formContainer.classList.toggle("hidden")
+})
